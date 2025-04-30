e1623736be7100541afb4a2e8e880a3d69e8f152
NguyenTanDung-2004 <22520001@gm.uit.edu.vn>
2025-04-30 12:31:59 +0700

    allow CORS

--- src/main/java/com/example/grown_together/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.grown_together.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.grown_together.entity.User;
-
-public interface UserRepository extends JpaRepository<User, String>{
-    
-}
